c5ab05d2c5fbb8c996e8dedc8c0b420cffbefc59
James Devaney <james.devaney@capgemini.com>
2017-08-18 14:44:16 +0100

    Fix JSON serialization problem

--- src/main/java/app/book/Book.java
+++ src/main/java/app/book/Book.java
@@ -7,12 +7,4 @@ public class Book {
     String title;
     String author;
     String isbn;
-
-    public String getMediumCover() {
-        return "http://covers.openlibrary.org/b/isbn/" + this.isbn + "-M.jpg";
-    }
-
-    public String getLargeCover() {
-        return "http://covers.openlibrary.org/b/isbn/" + this.isbn + "-L.jpg";
-    }
 }
