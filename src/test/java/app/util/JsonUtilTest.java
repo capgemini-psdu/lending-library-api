32654bcf0e9aca91e9c094b3bb41461ac99addfe
Vas <vasudev.ganesan@capgemini.com>
2018-05-14 16:26:27 +0100

    Changed jsonutiltest to use JSONObject to assert on the number of keys and the values generated

--- src/test/java/app/util/JsonUtilTest.java
+++ src/test/java/app/util/JsonUtilTest.java
@@ -2,17 +2,12 @@ package app.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import app.book.Book;
 
 public class JsonUtilTest {
-	
-	private static String EXPECTED_OUTPUT = "{\n" + 
-			"  \"title\" : \"Harry potter\",\n" + 
-			"  \"author\" : \"Rowling\",\n" + 
-			"  \"isbn\" : \"12345\"\n" + 
-			"}";
 	
 	@Test
 	 void testDataToJsonAndAssertOnOutput() {
@@ -21,7 +16,11 @@ public class JsonUtilTest {
 		//When
 		String outputJson = JsonUtil.dataToJson(sampleBook);
 		//Then
-		assertEquals(EXPECTED_OUTPUT,outputJson,"incorrect json output");
+		JSONObject jsonObject = new JSONObject(outputJson);
+		assertEquals(jsonObject.length(),3,"incorrect number of keys");
+		assertEquals(jsonObject.get("title"),"Harry potter","value for title is not correct");
+		assertEquals(jsonObject.get("author"),"Rowling","value for author is not correct");
+		assertEquals(jsonObject.get("isbn"),"12345","value for isbn is not correct");
 	}
 
 }
